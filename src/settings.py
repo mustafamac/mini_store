7aa4bade9e5902e3185e9605e35cfc99b744162e
Mustafa Hamdan <aimustafamac@gmail.com>
2026-04-25 07:27:38 +0300

    Fix database config

--- src/settings.py
+++ src/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # settings.py مؤقتًا
 # settings.py
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
